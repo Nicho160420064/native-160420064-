3910624345cef2055510f5e7961d38b5403d5b3e
Nicho160420064 <s160420064@student.ubaya.ac.id>
2023-02-28 18:33:50 +0800

    Tambah Activity change password

---
+++ app/src/main/java/com/ubaya/native160420064/ChangePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.ubaya.native160420064
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+
+        //change passowrd
+    }
+}
